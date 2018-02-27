8a11532bfd5cfd65a118de9cc1e00d0ae5ce03d5
Will Taylor <w1ll100@hotmail.co.uk>
2018-02-27 21:53:14 +0000

    Set up for comparing features implemented

--- main.py
+++ main.py
@@ -135,23 +135,37 @@ if __name__ == "__main__":
 	for i in range(k):
 		test_indices_lists.append([index for index in indices[i * slice_size: (i + 1) * slice_size]])
 
-	total_precision = 0
-	for test_indices in test_indices_lists:
-		train_indices = [n for n in indices if n not in test_indices]
-		test_text, train_text, test_labels, train_labels = df.TEXT[test_indices], df.TEXT[train_indices], df.LABEL[test_indices], df.LABEL[train_indices]
+	extractor_name_dict = { "tf"			: FeatureExtractor("tf"),
+							"tfidf" 		: FeatureExtractor("tfidf"),
+							"2gram tf"		: FeatureExtractor("tf", ngram_range = (2, 2)),
+							"2gram tfidf"	: FeatureExtractor("tfidf", ngram_range = (2, 2))}
 
-		print("Computing train tf")
-		feature_extractor = FeatureExtractor("tf")
+	for name, feature_extractor in extractor_name_dict.items():
+		print("Training and testing {} extractor with {}-fold cross validation".format(name, k))
+		avg_accuracy =  avg_precision =  avg_recall =  avg_fmeasure = 0
+		for test_indices in test_indices_lists:
+			train_indices = [n for n in indices if n not in test_indices]
+			test_text, train_text, test_labels, train_labels = df.TEXT[test_indices], df.TEXT[train_indices], df.LABEL[test_indices], df.LABEL[train_indices]
 
-		train_tf = feature_extractor.compute_tf_mat(train_text)
+			#print("Computing train tf")
+			train_tf = feature_extractor.compute_tf_mat(train_text)
 
-		print("Training Naive Bayes")
-		clf = MultinomialNB().fit(train_tf, train_labels)
+			#print("Training Naive Bayes")
+			clf = MultinomialNB().fit(train_tf, train_labels)
 
-		print("Computing test tf")
-		test_tf = feature_extractor.compute_tf_mat(test_text)
-		predictions = clf.predict(test_tf)
+			#print("Computing test tf")
+			test_tf = feature_extractor.compute_tf_mat(test_text)
+			predictions = clf.predict(test_tf)
 
-		accuracy, precision, recall, fmeasure = compute_metrics(predictions, test_labels)
-		print("{:10}: {}\n{:10}: {}\n{:10}: {}\n{:10}: {}\n".format("Accuracy", accuracy, "Precision", precision, "Recall", recall, "F-Measure", fmeasure))
-		
+			accuracy, precision, recall, fmeasure = compute_metrics(predictions, test_labels)
+			avg_accuracy += accuracy
+			avg_precision += precision
+			avg_recall += recall
+			avg_fmeasure += fmeasure
+			#print("{:10}: {}\n{:10}: {}\n{:10}: {}\n{:10}: {}\n".format("Accuracy", accuracy, "Precision", precision, "Recall", recall, "F-Measure", fmeasure))
+
+		avg_accuracy = avg_accuracy / k
+		avg_precision = avg_precision / k
+		avg_recall = avg_recall / k
+		avg_fmeasure = avg_fmeasure / k
+		print("Averages:\n--------------\n{:10}: {}\n{:10}: {}\n{:10}: {}\n{:10}: {}\n".format("Accuracy", avg_accuracy, "Precision", avg_precision, "Recall", avg_recall, "F-Measure", avg_fmeasure))
