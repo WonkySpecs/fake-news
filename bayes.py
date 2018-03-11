04d2ed7ce5e37eadf9c99da556bc5e7e11add6db
Will Taylor <w1ll100@hotmail.co.uk>
2018-03-11 19:16:02 +0000

    Fixed ngram range not actually being used

--- bayes.py
+++ bayes.py
@@ -1,10 +1,30 @@
 import random
 import math
 
-from common_functions import compute_metrics
-
 from sklearn.naive_bayes import MultinomialNB
 import sklearn.feature_extraction.text as sk
+
+#Input: list/array of predicted 0/1 labels and the actual labels
+#Outputs: accuracy, precision, recall and f-measure of predictions
+def compute_metrics(predictions, actual):
+	tp = tn = fp = fn = 0
+
+	for pred, ac in zip(predictions, actual):
+		if ac == 0:
+			if pred == 0:
+				tn += 1
+			else:
+				fp += 1
+		else:
+			if pred == 1:
+				tp += 1
+			else:
+				fn += 1
+	recall = tp / float(tp + fn)
+	precision = tp / float(tp + fp)
+
+	#Accuracy, precision, recall, f-measure
+	return (tp + tn) / len(predictions), precision, recall, 2 * (precision * recall) / (precision + recall)
 
 #Wrapper class for sk countvectorizer and tfidftransformer
 class FeatureExtractor:
@@ -19,36 +39,37 @@ class FeatureExtractor:
 			self.ngram_range = (1, 1)
 		else:
 			self.ngram_range = ngram_range
-		print(self.ngram_range)
 
 	def compute_freq_mat(self, input_texts):
 		if self.count_vect:
 			word_doc_freq_mat = self.count_vect.transform(input_texts)
 		else:
-			self.count_vect = sk.CountVectorizer(self.ngram_range)
+			self.count_vect = sk.CountVectorizer(ngram_range = self.ngram_range)
 			word_doc_freq_mat = self.count_vect.fit_transform(input_texts)
 
 		if self.feature_type == "tf":
-			tf_transformer = sk.TfidfTransformer(use_idf = False).fit(word_doc_freq_mat)
+			freq_transformer = sk.TfidfTransformer(use_idf = False).fit(word_doc_freq_mat)
 		else:
-			tf_transformer = sk.TfidfTransformer(use_idf = True).fit(word_doc_freq_mat)
+			freq_transformer = sk.TfidfTransformer(use_idf = True).fit(word_doc_freq_mat)
 
-		tf_mat = tf_transformer.transform(word_doc_freq_mat)
+		freq_mat = freq_transformer.transform(word_doc_freq_mat)
 
-		return tf_mat
+		return freq_mat
 
 def mn_bayes(df, k):
-	try_combinations = True
+	try_combinations = False
 	if try_combinations:
-		extractor_name_dict = { "tf"			: FeatureExtractor("tf"),
-								"tfidf" 		: FeatureExtractor("tfidf"),
-								"2gram tf"		: FeatureExtractor("tf", ngram_range = (2, 2)),
-								"2gram tfidf"	: FeatureExtractor("tfidf", ngram_range = (2, 2)),
-								"4gram tf"		: FeatureExtractor("tf", ngram_range = (4, 4)),
-								"4gram tfidf"	: FeatureExtractor("tfidf", ngram_range = (4, 4)),
-								"1-4gram tfidf"	: FeatureExtractor("tfidf", ngram_range = (1, 4))}
+		extractor_name_dict = { "tf"			: ("tf", (1, 1)),
+								"tfidf" 		: ("tfidf", (1, 1)),
+								"2gram tf"		: ("tf", (2, 2)),
+								"2gram tfidf"	: ("tfidf", (2, 2)),
+								"3gram tf"		: ("tf", (3, 3)),
+								"3gram tfidf"	: ("tfidf", (3, 3)),
+								"4gram tf"		: ("tf", (4, 4)),
+								"4gram tfidf"	: ("tfidf", (4, 4)),
+								"1-3gram tfidf"	: ("tfidf", (1, 3))}
 	else:
-		extractor_name_dict = {	"tfidf"			: FeatureExtractor("tfidf")}
+		extractor_name_dict = {	"3gram tfidf"	: ("tfidf", (3, 3))}
 
 	#Randomize test/training sets
 	indices = [n for n in range(len(df))]
@@ -59,21 +80,23 @@ def mn_bayes(df, k):
 	for i in range(k):
 		test_indices_lists.append([index for index in indices[i * slice_size: (i + 1) * slice_size]])
 
-	for name, feature_extractor in extractor_name_dict.items():
+	for name, (freq_type, ngram) in extractor_name_dict.items():
 		print("Training and testing {} extractor with {}-fold cross validation".format(name, k))
 		avg_accuracy =  avg_precision =  avg_recall =  avg_fmeasure = 0
 		for test_indices in test_indices_lists:
+			feature_extractor = FeatureExtractor(freq_type, ngram_range = ngram)
 			train_indices = [n for n in indices if n not in test_indices]
 			test_text, train_text, test_labels, train_labels = df.TEXT[test_indices], df.TEXT[train_indices], df.LABEL[test_indices], df.LABEL[train_indices]
 
 			train_freq_mat = feature_extractor.compute_freq_mat(train_text)
-
-			#print("Training Naive Bayes")
-			clf = MultinomialNB().fit(train_freq_mat, train_labels)
-
 			test_freq_mat = feature_extractor.compute_freq_mat(test_text)
-			predictions = clf.predict(test_freq_mat)
 
+			classifier = MultinomialNB()
+			#Train
+			classifier.fit(train_freq_mat, train_labels)
+			#Predict
+			predictions = classifier.predict(test_freq_mat)
+			#Evaluate
 			accuracy, precision, recall, fmeasure = compute_metrics(predictions, test_labels)
 			avg_accuracy += accuracy
 			avg_precision += precision
